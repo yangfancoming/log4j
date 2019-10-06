994286ee5658b051ce44c9873d5f037017016e5b
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-10-06 15:32:10 +0800

    代码格式优化

--- src/main/java/org/apache/log4j/EnhancedPatternLayout.java
+++ src/main/java/org/apache/log4j/EnhancedPatternLayout.java
@@ -528,6 +528,7 @@ public class EnhancedPatternLayout extends Layout {
 
   /**
    * Will return false if any of the conversion specifiers in the pattern handles {@link Exception Exceptions}.
+   *
    * @return true if the pattern formats any information from exceptions.
    */
   @Override
